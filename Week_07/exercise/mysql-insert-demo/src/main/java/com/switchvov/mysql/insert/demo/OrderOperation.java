ae73a6f904e124dbad9f03bb49e741b6beeb077a
switch <switchvov@163.com>
2021-04-30 15:04:45 +0800

    update homework week07

--- Week_07/exercise/mysql-insert-demo/src/main/java/com/switchvov/mysql/insert/demo/OrderOperation.java
+++ Week_07/exercise/mysql-insert-demo/src/main/java/com/switchvov/mysql/insert/demo/OrderOperation.java
@@ -43,6 +43,24 @@ public class OrderOperation implements Closeable {
         this.orders = Lists.partition(orders, 10000).get(0);
     }
 
+    /**
+     * CREATE TABLE `order_statement` (
+     *   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+     *   `order_id` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '订单ID',
+     *   `user_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '用户ID',
+     *   `sku_number` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'sku编码',
+     *   `total` decimal(12,2) NOT NULL DEFAULT '0.00' COMMENT '订单总金额',
+     *   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+     *   `create_by` int(11) NOT NULL DEFAULT '0' COMMENT '创建人',
+     *   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+     *   `update_by` int(11) NOT NULL DEFAULT '0' COMMENT '更新人',
+     *   PRIMARY KEY (`id`) USING BTREE,
+     *   KEY `idx_order_id` (`order_id`) USING BTREE,
+     *   KEY `idx_user_id` (`user_id`) USING BTREE,
+     *   KEY `idx_sku_number` (`sku_number`) USING BTREE
+     * ) ENGINE=InnoDB AUTO_INCREMENT=10001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='订单表(简化)';
+     * @return
+     */
     @SneakyThrows
     public boolean save() {
         Objects.requireNonNull(orders);
